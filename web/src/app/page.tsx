53192a417e7a6140fc379cae77f682a6453adf4c
rbutl0215 <rbutl0215@gmail.com>
2024-02-12 15:33:23 -0500

    logo buttons

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,18 +2,29 @@ import ExperienceCard from "@/components/ExperienceCard";
 import { experiences, projects } from "./contants";
 import Section from "@/components/Section";
 import ProjectCard from "@/components/ProjectCard";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="grid grid-cols-2 gap-2 h-screen">
       <div className="col-span-1 p-16 overflow-hidden">
         <h1 className="text-4xl font-bold mb-2">Reid Butler</h1>
-        <h2 className="text-2xl mb-2">Full-stack Software Engineer</h2>
+        <h2 className="text-2xl mb-2">Full Stack Software Engineer</h2>
         <text className="text-lg text-slate-400	">
           Self-taught hacker at heart, blending creative problem-solving with
           diverse experiences from factory floors to software development,
           fueled by a curiosity
         </text>
+        <div className="flex gap-4 py-4">
+          <Link href="https://github.com/rbutl0215">
+            <FontAwesomeIcon icon={faGithub} className="w-10 h-10"/>
+          </Link>
+          <Link href="https://www.linkedin.com/in/reid-butler-eng/">
+            <FontAwesomeIcon icon={faLinkedin} className="w-10 h-10"/>
+          </Link>
+        </div>
       </div>
       <div className="col-span-1 p-16 overflow-y-auto">
         <Section title="About">
